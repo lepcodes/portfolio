6898aeb1c8bed663d10db5ce97f56174fd735e22
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-03-30 00:38:43 -0600

    New feature of responsive design

--- src/components/chatbot/ChatHeader.tsx
+++ src/components/chatbot/ChatHeader.tsx
@@ -4,8 +4,8 @@ export default function ChatHeader( { className }: { className: string }) {
       <h2 className="text-3xl text-gray-400 text-center">
         Hi there! 👋 
       </h2>
-      <h1 className="text-7xl/22 font-semibold text-gray-200 text-center ">
-        Welcome, I'm<br/> <span className='text-gradient font-extrabold text-[5.5rem]'>Luis Preciado</span>
+      <h1 className="text-7xl/20 md:text-7xl/22 font-semibold text-gray-200 text-center ">
+        Welcome, I'm<br/> <span className='text-gradient font-extrabold md:text-[5.5rem]'>Luis Preciado</span>
       </h1>
       <p className="text-gray-300 text-center text-base w-[90%]">
         Explore my projects, learn about my skills, or contact me for more information.
