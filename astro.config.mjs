d82d7862375bb246173ae66d9482946c7de55aba
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-06-04 17:38:54 -0600

    Add vercel adapter to handle server output for POST requests

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  output: 'server',
+  integrations: [react()],
+  adapter: vercel()
 });
