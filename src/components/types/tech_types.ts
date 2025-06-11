b75e3a93a85b3db514bb36d7d1112e62c0844cb9
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-06-11 01:51:13 -0600

    Dummy information removed and TechTags included with dark and light theming

--- src/components/types/tech_types.ts
+++ src/components/types/tech_types.ts
@@ -1,53 +1,64 @@
 import { FaPython, FaJsSquare, FaReact, FaHtml5, FaCss3 } from 'react-icons/fa';
-import { SiFastapi, SiCmake } from 'react-icons/si';
+import { SiFastapi, SiCmake, SiNextdotjs } from 'react-icons/si';
 import { TbBrandPowershell } from "react-icons/tb";
+import { BiLogoTypescript } from "react-icons/bi";
 import ROSIcon from "../../icons/ROSIcon.astro";
 
 export const Technology = {
     PYTHON: {
       name: "Python",
-      color: "#4693d4",
+      color: ["#4693d4", "#4693d4"],
       icon: FaPython
     },
     JAVASCRIPT: {
       name: "JavaScript",
-      color: "#F7DF1E",
+      color: ["#F7DF1E", "#c7b212"],
       icon: FaJsSquare
+    },
+    TYPESCRIPT: {
+      name: "TypeScript",
+      color: ["#3178C6", "#3178C6"],
+      icon: BiLogoTypescript
     },
     REACT: {
       name: "React",
-      color: "#61DAFB",
+      color: ["#61DAFB", "#3196b1"],
       icon: FaReact
     },
     HTML: {
       name: "HTML",
-      color: "#E44D26",
+      color: ["#E44D26", "#E44D26"],
       icon: FaHtml5
     },
     CSS: {
       name: "CSS",
-      color: "#8c6eba",
+      color: ["#8c6eba", "#8c6eba"],
       icon: FaCss3
     },
     FASTAPI: {
       name: "FastAPI",
-      color: "#009688",
+      color: ["#009688", "#009688"],
       icon: SiFastapi
     },
     ROS: {
       name: "ROS",
-      color: "#9353C1",
+      color: ["#9353C1", "#9353C1"],
       icon: ROSIcon
     },
     SHELL: {
       name: "Shell Script",
-      color: "#009688",
+      color: ["#009688", "#009688"],
       icon: TbBrandPowershell
     },
     CMAKE: {
       name: "CMake",
-      color: "#C73C43",
+      color: ["#C73C43", "#C73C43"],
       icon: SiCmake
+    },
+    NEXTJS: {
+      name: "Next.js",
+      color: ["#ffffff", "#000000"],
+      icon: SiNextdotjs
     },
   }
   
